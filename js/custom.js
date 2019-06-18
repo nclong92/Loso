3a46b2a0187eb3c38375fef0d2efc8a479a241b5
longnc <nclong92@gmail.com>
2019-06-18 16:03:51 +0700

    auto padding to Header

--- js/custom.js
+++ js/custom.js
@@ -93,7 +93,26 @@ $(document).ready(function () {
     });
 });
 
+// add auto padding to header
+$(document).ready(function () {
+    'use strict';
 
+    setInterval(function () {
+        'use strict';
+
+        var windowHeight = $(window).height();
+
+        var containerHeight = $(".header-container").height();
+
+        var padTop = windowHeight - containerHeight;
+
+        $('.header-container').css({
+            'padding-top': Math.round(padTop / 2) + 'px',
+            'padding-bottom': Math.round(padTop / 2) + 'px'
+        });
+
+    }, 10);
+});
 
 
 
