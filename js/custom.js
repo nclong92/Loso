adb377423ac7040e1701bd6e6b125bc4e20b37cb
longnc <nclong92@gmail.com>
2019-06-18 16:25:35 +0700

    add slider to screens

--- js/custom.js
+++ js/custom.js
@@ -114,6 +114,15 @@ $(document).ready(function () {
     }, 10);
 });
 
-
+// Add bx slider to screens
+$(document).ready(function () {
+    $('.bxslider').bxSlider({
+        slideWidth: 292.5,
+        auto: true,
+        minSlides: 1,
+        maxSlides: 3,
+        slideMargin: 50
+    });
+});
 
 
