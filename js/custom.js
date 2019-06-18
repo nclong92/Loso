68a11cb77b5586f542ca47449e713092079bd905
longnc <nclong92@gmail.com>
2019-06-18 17:02:17 +0700

    add counter

--- js/custom.js
+++ js/custom.js
@@ -125,4 +125,11 @@ $(document).ready(function () {
     });
 });
 
+// Add counter
+$(document).ready(function(){
+    $('.counter-num').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
 
