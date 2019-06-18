670b8ec5f7c44304d780beed912154d0d283aa88
longnc <nclong92@gmail.com>
2019-06-18 15:46:21 +0700

    building navigation

---
+++ js/custom.js
@@ -0,0 +1,100 @@
+// show and hide menu
+$(document).ready(function () {
+    'use strict';
+    $(window).scroll(function () {
+        'use strict';
+
+        if ($(window).scrollTop() < 80) {
+            $('.navbar').css({
+                'margin-top': '-100px',
+                'opacity': '0'
+            });
+
+            $('.navbar-default').css({
+                'background-color': 'rgba(59, 59, 59, 0)'
+            });
+
+        } else {
+            $('.navbar').css({
+                'margin-top': '0px',
+                'opacity': '1'
+            });
+
+            $('.navbar-default').css({
+                'background-color': 'rgba(59, 59, 59, 0.7)',
+                'border-color': '#444'
+            });
+
+            $('.navbar-brand img').css({
+                'height': '35px',
+                'padding-top': '0px'
+            });
+
+            $('.navbar-brand > li > a').css({
+                'height': '15px'
+            });
+        }
+    })
+});
+
+// add smooth scrooling
+$(document).ready(function () {
+    'use strict';
+
+    $('.nav-item, #scroll-to-top').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+// active menu item on click
+$(document).ready(function () {
+    'use strict';
+
+    $('.navbar-nav li a').click(function () {
+        'use strict';
+
+        $('.navbar-nav li a').parent().removeClass("active");
+
+        $(this).parent().addClass("active");
+    })
+});
+
+// highlight menu item on scroll
+$(document).ready(function () {
+    'use strict';
+
+    $(window).scroll(function () {
+        'use strict';
+
+        $("section").each(function () {
+            'use strict';
+
+            var bb = $(this).attr("id");    // ABOUT, CONTACT, DOWNLOAD ...
+            var hei = $(this).outerHeight();
+            var grttop = $(this).offset().top - 70;
+
+            if ($(window).scrollTop() > grttop && $(window).scrollTop() < grttop + hei) {
+                $(".navbar-nav li a[href='#" + bb + "']").parent().addClass("active");
+            } else {
+                $(".navbar-nav li a[href='#" + bb + "']").parent().removeClass("active");
+            }
+        });
+    });
+});
+
+
+
+
+
+
