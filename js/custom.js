b2439ec93c19a477369e6a518f24dd3e5e6ed2ca
longnc <nclong92@gmail.com>
2019-06-19 08:53:35 +0700

    building animation

--- js/custom.js
+++ js/custom.js
@@ -126,10 +126,15 @@ $(document).ready(function () {
 });
 
 // Add counter
-$(document).ready(function(){
+$(document).ready(function () {
     $('.counter-num').counterUp({
         delay: 10,
         time: 2000
     });
 });
 
+// Add animation/ Initialize Woo
+$(document).ready(function () {
+    'use strict';
+    new WOW().init();
+});
